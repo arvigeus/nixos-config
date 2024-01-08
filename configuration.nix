ec80b2c99e47ca34c16ed67cfb38568e8268e704
Nikolay Stoynov <arvigeus@gmail.com>
2024-01-08 12:19:44 +0700

    feat: add fonts

--- configuration.nix
+++ configuration.nix
@@ -252,14 +252,32 @@
     pinentry-qt
   ];
   
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-  ];
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      corefonts # Microsoft free fonts
+      dejavu_fonts
+      inconsolata # monospaced
+      nerdfonts
+      source-code-pro
+      source-sans-pro
+      source-serif-pro
+      ubuntu_font_family
+      unifont # some international languages
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "Source Code Pro" ];
+        sansSerif = [ "Source Sans Pro" ];
+        serif = [ "Source Serif Pro" ];
+      };
+    };
+  };
 
   programs.firefox = {
     enable = true;
