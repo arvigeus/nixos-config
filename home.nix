ecb4c3aee3f7fd0de73fb3d38da74961d12f0cb9
Nikolay Stoynov <arvigeus@gmail.com>
2024-02-20 10:56:44 +0700

    fix(vscode): set global default formatter

--- home.nix
+++ home.nix
@@ -108,8 +108,10 @@
       "git.confirmSync" = false;
       "git.enableCommitSigning" = true;
 
+      "editor.defaultFormatter" = "esbenp.prettier-vscode";
       "[json].editor.defaultFormatter" = "esbenp.prettier-vscode";
       "[javascript].editor.defaultFormatter" = "esbenp.prettier-vscode";
+      "[typescript].editor.defaultFormatter" = "esbenp.prettier-vscode";
       "[typescriptreact].editor.defaultFormatter" = "esbenp.prettier-vscode";
       "[jsonc].editor.defaultFormatter" = "esbenp.prettier-vscode";
       "[markdown].editor.defaultFormatter" = "esbenp.prettier-vscode";
