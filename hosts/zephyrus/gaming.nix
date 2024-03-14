8243dce155d4cdf1b68fc833f118e7d241d386fe
Nikolay Stoynov <arvigeus@gmail.com>
2024-03-14 14:29:02 +0700

    fix(steam): remove steam-hardware Already enabled by programs.steam.enable

--- hosts/zephyrus/gaming.nix
+++ hosts/zephyrus/gaming.nix
@@ -24,7 +24,6 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
-  hardware.steam-hardware.enable = true;
 
   programs.gamemode.enable = true;
 }
