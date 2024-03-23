ba5043f154197264aed0ae09bd0d5890c2ca5de1
Nikolay Stoynov <arvigeus@gmail.com>
2024-03-23 17:52:31 +0700

    feat (mpv): configure mpv addresses #13

--- hosts/zephyrus/multimedia.home.nix
+++ hosts/zephyrus/multimedia.home.nix
@@ -5,19 +5,24 @@
 		enable = true;
 		package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override {}) { youtubeSupport = true; };
 		scripts = with pkgs.mpvScripts; [ mpris uosc thumbfast sponsorblock quality-menu ];
-		# config.force-window = "immediate";
+		defaultProfiles = [ "high-quality" ]; # https://github.com/mpv-player/mpv/blob/master/etc/builtin.conf
+		config = {
+			# https://github.com/mpv-player/mpv/blob/master/DOCS/man/options.rst
+			vo = "dmabuf-wayland"; # https://github.com/mpv-player/mpv/blob/master/DOCS/man/vo.rst
+			# vo = "gpu-next";
+			hwdec = "auto-safe";
+			gpu-api = "vulkan";
+			sub-auto = "fuzzy";
+			alang = "jpn,en";
+			slang = "en";
+		};
 		# bindings = {
 		# 	"+" = "add volume 2";
 		# 	"-" = "add volume -2";
 		# };
 		scriptOpts.uosc = {
-      timeline_size = 10;
-		# 	timeline_size_min = 1;
-		# 	timeline_persistency = "paused,audio";
+		# https://github.com/tomasklaen/uosc/blob/main/src/uosc.conf
 			controls = "menu,space,subtitles,<has_many_audio>audio,<has_many_video>video,<has_many_edition>editions,<stream>stream-quality";
-		# 	# volume = "none";
-		# 	window_border_size = 0;
-		# 	# pause_indicator = "none";
 		};
 	};
 }
