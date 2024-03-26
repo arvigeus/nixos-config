e12cfe907278216b9ef311ad442bb21301c666af
Nikolay Stoynov <arvigeus@gmail.com>
2024-03-26 17:54:39 +0700

    feat(chromium): add extensions

--- apps/chromium.nix
+++ apps/chromium.nix
@@ -1,5 +1,21 @@
 { ... }:
 
 {
-  programs.chromium.enable = true;
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock-origin
+      { id = "nngceckbapebfimnlniiiahkandclblb"; } # bitwarden-free-password-m
+      { id = "cimiefiiaegbelhefglklhhakcgmhkai"; } # plasma-integration
+      { id = "fmkadmapgofadopljbjfkapdkoienihi"; } # react-developer-tools
+      { id = "gppongmhjkpfnbhagpmjfkannfbllamg"; } # wappalyzer-technology-pro
+      { id = "gccemnpihkbgkdmoogenkbkckppadcag"; } # webhint
+      { id = "ponfpcnoihfmfllpaingbgckeeldkhle"; } # enhancer-for-youtube
+      { id = "cnojnbdhbhnkbcieeekonklommdnndci"; } # search-by-image
+      { id = "kbfnbcaeplbcioakkpcpgfkobkghlhen"; } # grammarly-ai-writing-and
+      { id = "bmnlcjabgnpnenekpadlanbbkooimhnj"; } # paypal-honey-automatic-co
+      { id = "pfldomphmndnmmhhlbekfbafifkkpnbc"; } # cultivate-amazon-product
+      { id = "ghnomdcacenbmilgjigehppbamfndblo"; } # the-camelizer
+    ];
+  };
 }
