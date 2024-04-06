c8dbcf149c6b53b1b8e4d412dda231f027dff09c
Nikolay Stoynov <arvigeus@gmail.com>
2024-04-06 23:22:06 +0700

    feat(bash): pretty ls

--- apps/bash.nix
+++ apps/bash.nix
@@ -1,7 +1,13 @@
-{...}: {
+{pkgs, ...}: {
+  home.packages = with pkgs; [eza];
+
   programs.bash = {
     enable = true;
     enableCompletion = true;
     historyControl = ["erasedups" "ignoredups"];
+    shellAliases = {
+      ls = "eza --icons -F -H --group-directories-first --git -1";
+      ll = "ls -alF";
+    };
   };
 }
