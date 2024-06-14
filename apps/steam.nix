c84f2382adb03c97d0a442aaf8fd819e100b024b
Nikolay Stoynov <arvigeus@gmail.com>
2024-06-14 12:21:45 +0700

    feat(steam): tweak stuff

--- apps/steam.nix
+++ apps/steam.nix
@@ -8,7 +8,38 @@
         with pkgs; [
           gamescope
           mangohud
+          # No idea what these are for
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
         ];
+    };
+  };
+
+  home-manager.users.arvigeus.xdg.desktopEntries = {
+    steamBigPicture = {
+      name = "Steam (Big Picture Mode)";
+      comment = "Application for managing and playing games on Steam (Big Picture Mode)";
+      exec = "steam -gamepadui -steamdeck %U";
+      terminal = false;
+      categories = ["Application"];
+      mimeType = [];
+    };
+
+    steam = {
+      name = "Steam";
+      comment = "Application for managing and playing games on Steam";
+      exec = "steam -forcedesktopscaling 1.25 %U";
+      terminal = false;
+      categories = ["Application"];
+      mimeType = [];
     };
   };
 }
