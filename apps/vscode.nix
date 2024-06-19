bfe257fdf4695b01e5dba7900618a76fe3ced45c
Nikolay Stoynov <arvigeus@gmail.com>
2024-06-19 22:10:24 +0700

    chore: update system

--- apps/vscode.nix
+++ apps/vscode.nix
@@ -25,7 +25,7 @@
         zhuangtongfa.material-theme
 
         # Web Dev
-        dbaeumer.vscode-eslint
+        # dbaeumer.vscode-eslint
         esbenp.prettier-vscode
         csstools.postcss
         stylelint.vscode-stylelint
@@ -60,6 +60,7 @@
       ]
       ++ (with pkgs.vscode-marketplace; [
         # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/vscode-marketplace-latest.json
+        dbaeumer.vscode-eslint
         mtxr.sqltools-driver-sqlite
         ms-vscode-remote.vscode-remote-extensionpack
         ms-vscode.remote-explorer
